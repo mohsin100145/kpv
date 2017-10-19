f9c81e4b01403cf89317f60261f16411afeca3c7
mohsin100145 <mohsin.cse.45@gmail.com>
2017-10-20 00:35:56 +0600

    sth

---
+++ database/migrations/2017_10_18_155806_create_payable_by_customers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePayableByCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payable_by_customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('reservation_id')->unsigned();
+            $table->integer('day')->unsigned();
+            $table->float('per_day_discount')->nullable();
+            $table->float('per_day_discount_percentage')->nullable();
+            $table->float('overall_discount')->nullable();
+            $table->float('vat')->nullable();
+            $table->float('other_charge')->nullable();
+            $table->float('pay_to_hotel');
+            $table->string('remarks')->nullable();
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payable_by_customers');
+    }
+}
